89bdb44b4cb6c7f96da1cc53482593817c945106
rockymoto517 <74614475+rockymoto517@users.noreply.github.com>
2025-02-10 14:24:04 -0600

    Fix skill cost 3 final (for real this time)

--- public/js/Builder.js
+++ public/js/Builder.js
@@ -283,9 +283,9 @@ export default class Builder {
      */
     getSkillTierCosts() {
         if (this.exp.infamyDisabled){
-            return [0, 1, 3, 13]; // 18 total points for T4 if no infamy
+            return [0, 1, 3, 12]; // 18 total points for T4 if no infamy
         } else {
-            return [0, 1, 3, 13]; // 16 total points for T4 if infamy
+            return [0, 1, 3, 12]; // 16 total points for T4 if infamy
         }
     }
 
