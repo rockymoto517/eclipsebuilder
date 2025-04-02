ff32578e8d9ff8015849288a187fa090f8d8ca55
rockymoto517 <rockymoto517@gmail.com>
2025-04-02 12:01:17 -0500

    Add missing preview file, update packages

---
+++ preview.js
@@ -0,0 +1,21 @@
+const readline = require("readline");
+const { stdin, stdout, exit } = require("process");
+const { Worker } = require("worker_threads");
+
+const server = new Worker("./pd2builder.js");
+
+const rl = readline.createInterface({
+	input: stdin,
+	output: stdout,
+});
+
+rl.question("Enter (q) to quit.\n", (input) => {
+	if (input.trim().toLowerCase() === "q") {
+		console.log("Exiting...\n");
+		rl.close();
+		exit();
+	} else {
+		console.log("I'm stuff\n")
+	}
+})
+
