8bd614dceb698e36c7acb24d6b24e15a1a574174
rockymoto517 <rockymoto517@gmail.com>
2025-05-06 18:06:44 -0500

    Rogue rework; fix typos; add perk deck description test

--- test/db-localization.test.js
+++ test/db-localization.test.js
@@ -28,3 +28,30 @@ describe("English localization agrees with the Skill DB", function() {
         });
     });
 });
+
+describe("English localization agrees with the deck DB", function() {
+    fs.readFile("./public/db/perk_cards.json", "utf8", (e, dbData) => {
+        if (e) {
+            console.error("Error:", e);
+            return;
+        }
+
+        const cards = JSON.parse(dbData);
+        fs.readFile("./public/lang/en-us.json", "utf8", (e2, locData) => {
+            if (e2) {
+                console.error("Error:", e2);
+                return;
+            }
+
+            const locCards = JSON.parse(locData).perk_cards;
+            for (const key in cards) {
+                describe(key, function() {
+                    it("should match in en-us.json and perk_cards.json", function() {
+                        assert.strictEqual(cards[key].name, locCards[key].name);
+                        assert.strictEqual(cards[key].description, locCards[key].description);
+                    });
+                });
+            }
+        });
+    });
+});
