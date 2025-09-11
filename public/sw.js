ffef8d7f266b3521ee9297120416b3728370b843
rockymoto517 <rockymoto517@gmail.com>
2025-09-10 21:09:10 -0500

    Tank rework and other stuff and shit

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
-const version = "v2.3.2"; //change version each master release
-const store = ["/","index.html","version","mobile.html","css/common.css","css/desktop.css","css/fonts.css","css/fonts/cc.ttf","css/fonts/dinengschrift.ttf","css/fonts/padaloma.ttf","css/offsets.css","css/pd2table.css","db/ar.json","db/armors.json","db/deployables.json","db/perk_cards.json","db/perk_decks.json","db/primaries.json","db/shot_specials.json","db/skills.json","db/throwables.json","img/armors/ballistic.png","img/armors/ctv.png","img/armors/flak.png","img/armors/heavy_ballistic.png","img/armors/ictv.png","img/armors/light_ballistic.png","img/armors/suit.png","img/bg.png","img/borders/border.png","img/borders/corner-bottomleft.png","img/borders/corner-bottomright.png","img/borders/corner-topleft.png","img/borders/corner-topright.png","img/borders/skill_border.png","img/crews/deployables.png","img/crews/perks.png","img/deployables/ammo_bag.png","img/deployables/armor_kit.png","img/deployables/bodybags_bag.png","img/deployables/doctor_bag.png","img/deployables/ecm_jammer.png","img/deployables/first_aid_kit.png","img/deployables/sentry_gun.png","img/deployables/sentry_gun_silent.png","img/deployables/trip_mine.png","img/dropdown-arrow.png","img/favicon.png","img/lock_skill.png","img/masks/colors_icons.png","img/masks/colors_ui.png","img/perks/card.png","img/perks/icons.png","img/risk.png","img/skills/ace.png","img/skills/background.png","img/skills/background-90deg.png","img/skills/icons.png","img/skills/icons_atlas.png","img/skills/icons_small.png","img/skills/main.png","img/skills/padlock.png","img/throwables/ace_of_spades.png","img/throwables/concussion_grenade.png","img/throwables/dynamite.png","img/throwables/frag_grenade.png","img/throwables/gas_dispenser.png","img/throwables/hef_frag_grenade.png","img/throwables/incendiary_grenade.png","img/throwables/injector.png","img/throwables/javelin.png","img/throwables/leech_ampule.png","img/throwables/matryushka_grenade.png","img/throwables/molotov.png","img/throwables/pocket_ecm.png","img/throwables/shuriken.png","img/throwables/smoke_bomb.png","img/throwables/stoic_hip_flask.png","img/throwables/throwing_axe.png","img/throwables/throwing_knife.png","img/throwables/x1_zapper.png","js/Builder.js","js/GUI.js","js/index.js","js/IO.js","js/Language.js","js/mask.js","js/MaskControls.js","js/PaydayTable.js","js/Stats.js","js/Util.js","lang/en-us.json","lang/es-es.json","lang/fil-ph.json","lang/ru-ru.json","lang/zh-cn.json","manifest.webmanifest"];
+const version = "v2.3.3"; //change version each master release
+const store = ["/","index.html","mobile.html","css/common.css","css/desktop.css","css/fonts.css","css/fonts/cc.ttf","css/fonts/dinengschrift.ttf","css/fonts/padaloma.ttf","css/offsets.css","css/pd2table.css","db/ar.json","db/armors.json","db/deployables.json","db/perk_cards.json","db/perk_decks.json","db/primaries.json","db/shot_specials.json","db/skills.json","db/throwables.json","img/armors/ballistic.png","img/armors/ctv.png","img/armors/flak.png","img/armors/heavy_ballistic.png","img/armors/ictv.png","img/armors/light_ballistic.png","img/armors/suit.png","img/bg.png","img/borders/border.png","img/borders/corner-bottomleft.png","img/borders/corner-bottomright.png","img/borders/corner-topleft.png","img/borders/corner-topright.png","img/borders/skill_border.png","img/crews/deployables.png","img/crews/perks.png","img/deployables/ammo_bag.png","img/deployables/armor_kit.png","img/deployables/bodybags_bag.png","img/deployables/doctor_bag.png","img/deployables/ecm_jammer.png","img/deployables/first_aid_kit.png","img/deployables/sentry_gun.png","img/deployables/sentry_gun_silent.png","img/deployables/trip_mine.png","img/dropdown-arrow.png","img/favicon.png","img/lock_skill.png","img/masks/colors_icons.png","img/masks/colors_ui.png","img/perks/card.png","img/perks/icons.png","img/risk.png","img/skills/ace.png","img/skills/background.png","img/skills/background-90deg.png","img/skills/icons.png","img/skills/icons_atlas.png","img/skills/icons_small.png","img/skills/main.png","img/skills/padlock.png","img/throwables/ace_of_spades.png","img/throwables/concussion_grenade.png","img/throwables/dynamite.png","img/throwables/frag_grenade.png","img/throwables/gas_dispenser.png","img/throwables/hef_frag_grenade.png","img/throwables/incendiary_grenade.png","img/throwables/injector.png","img/throwables/javelin.png","img/throwables/leech_ampule.png","img/throwables/matryushka_grenade.png","img/throwables/molotov.png","img/throwables/pocket_ecm.png","img/throwables/shuriken.png","img/throwables/smoke_bomb.png","img/throwables/stoic_hip_flask.png","img/throwables/throwing_axe.png","img/throwables/throwing_knife.png","img/throwables/x1_zapper.png","js/Builder.js","js/GUI.js","js/index.js","js/IO.js","js/Language.js","js/mask.js","js/MaskControls.js","js/PaydayTable.js","js/Stats.js","js/Util.js","lang/en-us.json","lang/es-es.json","lang/fil-ph.json","lang/ru-ru.json","lang/zh-cn.json","manifest.webmanifest"];
 
-function generateCache(ev) {
+self.addEventListener("install", ev => {
     self.skipWaiting();
     ev.waitUntil(
         caches.open("v1").then(cache => 
@@ -14,10 +14,6 @@ function generateCache(ev) {
             cache.addAll(store)
         );
     }, 15 * 60 * 1000);
-}
-
-self.addEventListener("install", ev => {
-    generateCache(ev);
 });
 
 self.addEventListener("fetch", ev => {
@@ -25,16 +21,6 @@ self.addEventListener("fetch", ev => {
 });
 
 async function checkCacheOrFetch(ev) {
-    const res_version = await (await caches.open("v1")).match("version", { ignoreSearch: true });
-    if (res_version === undefined) {
-        generateCache(ev);
-    } else {
-        const cache_version = (await res_version.text()).trim();
-        if (cache_version !== version) {
-            generateCache(ev);
-        }
-    }
-
     const match = await (await caches.open("v1")).match(ev.request, { ignoreSearch: true });
     if(match === undefined) {
         return fetch(ev.request.clone())
