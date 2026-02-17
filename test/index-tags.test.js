7d375416cac336d5dd43d4dea2026cf6690a9a7a
rockymoto517 <rockymoto517@gmail.com>
2026-02-16 18:27:05 -0600

    bg threat

--- test/index-tags.test.js
+++ test/index-tags.test.js
@@ -5,35 +5,35 @@ const { JSDOM } = require("jsdom");
 
 // Make sure that all skills displayed on the desktop
 // website have an entry in the skill database
-describe("Skills", function() {
-    it("should have entries in the DOM", function(done) {
-        JSDOM.fromFile("./pages/index.html").then(dom => {
-            const document = dom.window.document;
-            const spans = document.getElementsByClassName("sk_name");
+describe("Skills", function () {
+  it("should have entries in the DOM", function (done) {
+    JSDOM.fromFile("./pages/index.html").then((dom) => {
+      const document = dom.window.document;
+      const spans = document.getElementsByClassName("sk_name");
 
-            fs.readFile("./public/db/skills.json", "utf8", (e, data) => {
-                if (e) {
-                    console.error("Error:", e);
-                    done(e);
-                    return;
-                }
+      fs.readFile("./public/db/skills.json", "utf8", (e, data) => {
+        if (e) {
+          console.error("Error:", e);
+          done(e);
+          return;
+        }
 
-                const skills = JSON.parse(data);
-                for (let span of spans) {
-                    const skill = span.innerHTML.toLowerCase()
-                        .replaceAll(" ", "_")
-                        .replaceAll("-", "_") // Mag-fed Specialist
-                        .replaceAll("'", ""); // Lock n' Load
-                    describe(skill, function() {
-                        it("should have an entry in the skill db", function() {
-                            if (skill !== "work_in_progress") {
-                                assert.ok(skills[skill]);
-                            }
-                        });
-                    });	
-                }
+        const skills = JSON.parse(data);
+        for (let span of spans) {
+          const skill = span.innerHTML
+            .toLowerCase()
+            .replaceAll(" ", "_")
+            .replaceAll("'", ""); // Lock n' Load
+          describe(skill, function () {
+            it("should have an entry in the skill db", function () {
+              if (skill !== "work_in_progress") {
+                assert.ok(skills[skill]);
+              }
             });
-            done();
-        });
+          });
+        }
+      });
+      done();
     });
+  });
 });
